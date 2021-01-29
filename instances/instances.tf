26920050210a63aab982470b3475c1e134fe77ac
Ioannis Moustakis <moustakisioannis@gmail.com>
2021-01-29 20:42:44 +0100

    Dummy PR (#1)
    * Modify ami
    
    * Restructure repo
    
    * Remove dummy remote backend

--- instances/instances.tf
+++ instances/instances.tf
@@ -1,8 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 resource "aws_instance" "my-instance" {
-  ami           = "ami-1234"
+  ami           = "ami-12345"
   instance_type = "t2.micro"
 }
